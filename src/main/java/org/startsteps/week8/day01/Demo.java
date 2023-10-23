d286317ed4dd97a0c02867095e3df32d6f7d7ae5
Faruk Arnaut <arnaut551@gmail.com>
2023-10-23 09:37:12 +0200

    Added demo file for week 8, day 1

---
+++ src/main/java/org/startsteps/week8/day01/Demo.java
@@ -0,0 +1,29 @@
+package org.startsteps.week8.day01;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello, ");
+        System.out.print("Startsteps");
+        System.out.print("! \n");
+
+        System.out.println("*********");
+
+
+        int x = 150;
+        System.out.printf("Printing simple integer x: %d\n", x);
+
+
+        System.out.printf("Formatted with precision: PI = %.2f\n", Math.PI);
+
+
+        float n = 5.2f;
+        System.out.printf("Formatted to specific width: n = %.4f\n", n);
+
+        n = 2324435.3f;
+        System.out.printf("Formatted to right margin: n = %20.4f\n", n);
+
+
+    }
+}
