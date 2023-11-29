12c79f1c640d73417ac61f58d46f3a2e5d18b5aa
Faruk Arnaut <arnaut551@gmail.com>
2023-11-29 12:24:07 +0100

    Added code for Week 13, Day 03 restful API

---
+++ src/main/java/org/startsteps/week13/DBConnection.java
@@ -0,0 +1,12 @@
+package org.startsteps.week13;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/practice", "root", "Password123.");
+    }
+}
+
---
+++ src/main/java/org/startsteps/week13/Main.java
@@ -0,0 +1,18 @@
+package org.startsteps.week13;
+
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String BASE_URI = "http://localhost:8080/";
+        ResourceConfig resourceConfig = new ResourceConfig(UserResource.class);
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
+        System.out.println("Server started at: " + BASE_URI);
+
+
+    }
+}
